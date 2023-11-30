735a7a7196a9840768e566dcb1465429203b0731
tieph2 <108648438+tieph2@users.noreply.github.com>
2023-11-29 21:56:24 -0800

    Added funtionality to get animal image url.

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -8,9 +8,10 @@ use axum::{
 use reqwest::{
     Client,
     ClientBuilder,
-    Error,
+    Error as RError,
     header::{HeaderMap, HeaderValue},
 };
+
 
 use csv::Reader;
 use rand::Rng;
@@ -19,29 +20,45 @@ use select::predicate::Name;
 use std::error;
 use std::fs::File;
 use std::path::Path;
-use vi_zoo::{AnimalQuery, AnimalRecord};
+use vi_zoo::{AnimalQuery, AnimalRecord, Animal, AnimalResponse};
+use serde::{Deserialize, Serialize};
+use image_search::{Arguments, Color, urls, search, download};
+
+
 
 const ANIMAL_URL: &str = "https://api.api-ninjas.com/v1/animals?name=";
 const CAT_URL: &str = "https://catfact.ninja/fact";
 const API_KEY : &str = "LX8eq5FkHB438N3K7ukLCw==DQTKEHV5lLP5rVtO";
+const IMG_API_KEY : &str= "AIzaSyAdGyXSOVw6-1hbLLNHtaOpV5yoZEObDB8";
 
-// fn fetch_image_url(search_query: &str) -> Result<String, reqwest::Error> {
+async fn get_url(animal: String) -> Result<(), image_search::Error> {
+    let args = Arguments::new(animal.as_str(), 10);
+    let _image_urls = urls(args.clone()).await?;
+    println!("{:?}", _image_urls);
+
+    Ok(())
+}
+
+
+// ///This function scrape Google image search to get image url of an animal
+// async fn fetch_image_url(search_query: &str) -> Result<String, reqwest::Error> {
 //     let client = Client::new();
 //     let search_url = format!("https://www.google.com/search?tbm=isch&q={}", search_query);
 //
-//     let response = client.get(&search_url).send();
-//     let body = response.text();
+//     let response = client.get(&search_url).send().await?;
+//     let body = response.text().await?;
 //
+//     let mut url = String::new();
 //     let document = Document::from(body.as_str());
 //     let img_element = document.find(Name("img")).next();
 //
 //     if let Some(img) = img_element {
 //         if let Some(img_url) = img.attr("src") {
-//             return Ok(img_url.to_string());
+//             url = img_url.to_string();
 //         }
 //     }
 //
-//     Err(Error)
+//     Ok(url)
 // }
 
 
@@ -73,9 +90,6 @@ pub async fn handler_rand_cat() -> Result<String, StatusCode> {
 }
 
 async fn read_random_record(file_path: &str) -> Result<csv::StringRecord, Box<dyn std::error::Error>> {
-    println!("Is this even being called at all?");
-
-
     // Open the CSV file
     let file = File::open(file_path)?;
 
@@ -91,7 +105,6 @@ async fn read_random_record(file_path: &str) -> Result<csv::StringRecord, Box<dy
 
 
     for result in reader.records() {
-        println!("{:?}", result);
         // Handle each CSV record
         match result {
             Ok(record) => {
@@ -119,7 +132,7 @@ async fn search_animal(name: &str) -> Result<String, StatusCode> {
     //Construct the header for the request with the API key
     let mut headers = HeaderMap::new();
     headers.insert("X-Api-Key", HeaderValue::from_str(API_KEY).unwrap());
-
+    println!("{}", name);
     let url = [ANIMAL_URL, name].concat();
     println!("{}", url);
 
@@ -137,7 +150,6 @@ async fn search_animal(name: &str) -> Result<String, StatusCode> {
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    println!("{}", body);
     if body == "[]" {
         println!("Empty response body");
     }
@@ -166,9 +178,7 @@ pub async fn handler_random() -> Result<String, StatusCode> {
     let mut name = String::new();
     match animal.await {
         Ok(record) => {
-            println!("{:?}", record);
             let my_struct: AnimalRecord = record.deserialize(None).unwrap();
-            println!("{:?}", my_struct);
             name = record.get(0).unwrap().to_string();
         }
         _ => {
@@ -179,9 +189,37 @@ pub async fn handler_random() -> Result<String, StatusCode> {
 
     let res = search_animal(name.as_str()).await;
 
+
     match res {
-        Ok(body) => return Ok(body),
+        Ok(body) => {
+            let animals : Vec<Animal> = serde_json::from_str(body.as_str()).expect("Failed to deserialize JSON");
+            for animal in animals {
+                let res = parse_animal(&animal).await;
+            }
+            return Ok(body)
+        },
         Err(e) => Err(e)
     }
 
+}
+
+async fn parse_animal(animal : &Animal) -> Result<AnimalResponse, Box<dyn std::error::Error>> {
+    // let url = fetch_image_url(animal.name.as_ref().unwrap().as_str()).await?;
+
+    println!("Getting url");
+    get_url(animal.name.clone().unwrap()).await?;
+
+    let response = AnimalResponse  {
+        name: "".to_string(),
+        scientific_name: "".to_string(),
+        locations: vec!["blue".to_string(), "red".to_string()],
+        prey: "".to_string(),
+        diet: "".to_string(),
+        r#type: "".to_string(),
+        color: "".to_string(),
+        lifespan: "".to_string(),
+        weight: "".to_string(),
+        img_url: "".to_string()
+    };
+    Ok(response)
 }
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -24,3 +24,60 @@ pub struct AnimalRecord {
     social_structure: String,
     offsprings: String
 }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Taxonomy {
+    pub kingdom: Option<String>,
+    pub phylum: Option<String>,
+    pub class: Option<String>,
+    pub order: Option<String>,
+    pub family: Option<String>,
+    pub genus: Option<String>,
+    pub scientific_name: Option<String>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Characteristics {
+    pub prey: Option<String>,
+    pub group_behavior: Option<String>,
+    pub estimated_population_size: Option<String>,
+    pub biggest_threat: Option<String>,
+    pub most_distinctive_feature: Option<String>,
+    pub other_names: Option<String>,
+    pub water_type: Option<String>,
+    pub habitat: Option<String>,
+    pub predators: Option<String>,
+    pub diet: Option<String>,
+    pub r#type: Option<String>,
+    pub common_name: Option<String>,
+    pub number_of_species: Option<String>,
+    pub color: Option<String>,
+    pub skin_type: Option<String>,
+    pub lifespan: Option<String>,
+    pub weight: Option<String>,
+    pub length: Option<String>,
+    pub height: Option<String>,
+    pub average_litter_size: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Animal {
+    pub name: Option<String>,
+    pub taxonomy: Taxonomy,
+    pub locations: Vec<String>,
+    pub characteristics: Characteristics,
+}
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AnimalResponse {
+    pub name: String,
+    pub scientific_name: String,
+    pub locations: Vec<String>,
+    pub prey: String,
+    pub diet: String,
+    pub r#type: String,
+    pub color: String,
+    pub lifespan: String,
+    pub weight: String,
+    pub img_url: String
+}
